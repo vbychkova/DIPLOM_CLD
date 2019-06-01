67e48f209172d4d5399446e3eb1b59ad00d52523
Valeryia Bychkova <valerybychkova@tut.by>
2019-06-01 16:14:28 +0300

    Deleting old files

--- js/functions/hardCodedElements.js
+++
@@ -1,9 +0,0 @@
-var space1 = state(180, 390, '');
-var space2 = state(600, 400, '');
-link(space1, space2, WHITE_SPACE, '', '');
-
-setTimeout(function () {
-    graph.clear();
-
-},1000);
-
--- js/functions/zoomActions.js
+++
@@ -1,6 +0,0 @@
-paper.on('blank:pointerdown', function () {
-    zoom.enablePan();
-});
-paper.on('cell:pointerup blank:pointerup', function () {
-    zoom.disablePan();
-});
